9c6ebcebfa939caca9041a8b397d75ef440a8733
lestat.de.lionkur <lestat.de.lionkur@gmail.org>
2009-10-27 20:01:52 +0200

    Matrix: ??????? ?????? ???????? ????????? ??????? ?? ???????

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,6 +1,8 @@
 #include "Matrix.h"
 Matrix & Matrix::operator = (const Matrix& arg) {
   if (this == &arg) return *this;
+// return left matrix if there is some error
+  if (this->error == true || arg.error == true) return *this;
   this->matrix.line = arg.matrix.line;
   this->matrix.column = arg.matrix.column;
   unsigned j = this->matrix.line * this->matrix.column;
@@ -10,6 +12,8 @@ Matrix & Matrix::operator = (const Matrix& arg) {
   return *this;
 }
 bool Matrix::operator == (const Matrix& arg) {
+// return false if there is some error
+  if (this->error == true || arg.error == true) return false;
   if (this->matrix.line != arg.matrix.line &&
   this->matrix.column != arg.matrix.column)
     return false;
@@ -20,18 +24,24 @@ bool Matrix::operator == (const Matrix& arg) {
   return true;
 }
 bool Matrix::operator != (const Matrix& arg) {
+// return false if there is some error
+  if (this->error == true || arg.error == true) return false;
   return !(*this == arg);
 }
 Matrix & Matrix::operator * (const double number) {
   return *this *= number;
 }
 Matrix & Matrix::operator *= (const double number) {
+// return left matrix if there is some error
+  if (this->error == true) return *this;
   int j = this->matrix.line*this->matrix.column;
   for (int i = 0; i < j; i++)
     this->matrix.matrix[i] *= number;
   return *this;
 }
 Matrix & Matrix::operator += (const Matrix& arg) {
+// return false if there is some error
+  if (this->error == true || arg.error == true) return *this;
   if ((this->matrix.line != arg.matrix.line) ||
      (this->matrix.column != arg.matrix.column)) {
     this->error = true;
@@ -46,6 +56,8 @@ Matrix & Matrix::operator + (const Matrix& arg) {
   return *this += arg;
 }
 Matrix & Matrix::operator -= (const Matrix& arg) {
+// return false if there is some error
+  if (this->error == true || arg.error == true) return *this;
   if ((this->matrix.line != arg.matrix.line) ||
      (this->matrix.column != arg.matrix.column)) {
     this->error = true;
@@ -58,4 +70,32 @@ Matrix & Matrix::operator -= (const Matrix& arg) {
 }
 Matrix & Matrix::operator - (const Matrix& arg) {
   return *this -= arg;
+}
+Matrix & Matrix::operator *= (const Matrix& arg) {
+// return false if there is some error
+  if (this->error == true || arg.error == true) return *this;
+  if (this->matrix.column != arg.matrix.line) {
+  this->error = true;
+   return *this;
+  }
+// Create new matrix for result
+  Matrix res;
+  res.matrix.line = arg.matrix.line;
+  res.matrix.column = this->matrix.column;
+  res.matrix.matrix = new double [res.matrix.line * res.matrix.column];
+  res.error = false;
+  double sum;
+  for (int i = 0; i < res.matrix.line; i++) {
+    for (int j = 0; j < res.matrix.column; j++) {
+      sum = 0;
+      for (int n = 0; n < arg.matrix.column; n++)
+        sum += this->matrix.matrix[i*this->matrix.column+n]*arg.matrix.matrix[n*arg.matrix.column+j];
+      res.matrix.matrix[i*res.matrix.column+j] = sum;
+    }
+  }
+  *this = res;
+  return *this;
+}
+Matrix & Matrix::operator * (const Matrix& arg) {
+  return *this *= arg;
 }
