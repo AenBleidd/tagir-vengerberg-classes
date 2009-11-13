70b8a20030625d3025beb1a11a8d035f237afdd8
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2009-11-13 16:31:06 +0200

    Matrix: ?????????? ??????? ?????????? ??????? ? ????????????. ???????? ???????? ??????? ???????? ???? ??????. ???? ?????? ??? ?? ???????????

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -1,24 +1,37 @@
 #ifndef _MATRIX_H_
 #define _MATRIX_H_
+#include <math.h>
 struct MATRIX {
   unsigned int line; // count of lines
   unsigned int column; // count of columns
-  double *matrix; // pointer to matrix array
+  long double *matrix; // pointer to matrix array
+};
+struct ERRORCODE {
+  unsigned short int operr;
+  unsigned short int ferr;
+  unsigned short int reserved0;
+  unsigned short int reserved1;
 };
 class Matrix {
  public:
+  Matrix();
   MATRIX matrix; // matrix structure
   bool error;
 // functions
+  unsigned long int GetLastError();
   Matrix* Trans (void);
+  Matrix* Minor (void);
+  Matrix* Minor (unsigned int line, unsigned int column);
+  long double MinorAux (unsigned int line, unsigned int column);
+  long double Det(void);
 // overloaded operators
   Matrix & operator = (const Matrix& arg);
   bool operator == (const Matrix& arg);
   bool operator != (const Matrix& arg);
-  Matrix & operator * (const double number);
-  Matrix & operator *= (const double number);
-  Matrix & operator / (const double number);
-  Matrix & operator /= (const double number);
+  Matrix & operator * (const long double number);
+  Matrix & operator *= (const long double number);
+  Matrix & operator / (const long double number);
+  Matrix & operator /= (const long double number);
   Matrix & operator + (const Matrix& arg);
   Matrix & operator += (const Matrix& arg);
   Matrix & operator - (const Matrix& arg);
@@ -27,5 +40,7 @@ class Matrix {
   Matrix & operator *= (const Matrix& arg);
   Matrix & operator / (const Matrix& arg);
   Matrix & operator /= (const Matrix& arg);
+ private:
+  ERRORCODE errorcode;
 };
 #endif
