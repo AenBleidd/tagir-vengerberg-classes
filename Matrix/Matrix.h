5ff0dccc42de35540d7c3835d7392b27c4257deb
lawliet <lawliet@linux-h33p.site>
2009-11-17 22:32:09 +0200

    Matrix: ?????????? ????? ??????

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -7,10 +7,10 @@ struct MATRIX {
   long double *matrix; // pointer to matrix array
 };
 struct ERRORCODE {
-  unsigned short int operr;
-  unsigned short int ferr;
-  unsigned short int reserved0;
-  unsigned short int reserved1;
+  unsigned short int operr; // operator errorcode
+  unsigned short int ferr; // function errorcode
+  unsigned short int reserved0; // reserved
+  unsigned short int reserved1; // reserved
 };
 class Matrix {
  public:
@@ -19,27 +19,27 @@ class Matrix {
   bool error;
 // functions
   unsigned long int GetLastError();
-  Matrix* Trans (void);
-  Matrix* Minor (void);
-  Matrix* Minor (unsigned int line, unsigned int column);
-  long double MinorAux (unsigned int line, unsigned int column);
-  long double Det(void);
+  Matrix* Trans (void); // reserved errorcode 1
+  Matrix* Minor (void); // errorcode 2
+  Matrix* Minor (unsigned int line, unsigned int column); // errorcode 4
+  long double MinorAux (unsigned int line, unsigned int column); // errorcode 8
+  long double Det(void); // errorcode 16
 // overloaded operators
-  Matrix & operator = (const Matrix& arg);
-  bool operator == (const Matrix& arg);
-  bool operator != (const Matrix& arg);
-  Matrix & operator * (const long double number);
-  Matrix & operator *= (const long double number);
-  Matrix & operator / (const long double number);
-  Matrix & operator /= (const long double number);
-  Matrix & operator + (const Matrix& arg);
-  Matrix & operator += (const Matrix& arg);
-  Matrix & operator - (const Matrix& arg);
-  Matrix & operator -= (const Matrix& arg);
-  Matrix & operator * (const Matrix& arg);
-  Matrix & operator *= (const Matrix& arg);
-  Matrix & operator / (const Matrix& arg);
-  Matrix & operator /= (const Matrix& arg);
+  Matrix & operator = (const Matrix& arg); // reserved errorcode 1
+  bool operator == (const Matrix& arg); // errorcode 2
+  bool operator != (const Matrix& arg); // errorcode 2
+  Matrix & operator * (const long double number); // reserved errorcode 4
+  Matrix & operator *= (const long double number); // reserved errorcode 4
+  Matrix & operator / (const long double number); // reserved errorcode 8
+  Matrix & operator /= (const long double number); // reserved errorcode 8
+  Matrix & operator + (const Matrix& arg); // errorcode 16
+  Matrix & operator += (const Matrix& arg); // errorcode 16
+  Matrix & operator - (const Matrix& arg); // errorcode 32
+  Matrix & operator -= (const Matrix& arg); // errorcode 32
+  Matrix & operator * (const Matrix& arg); // errorcode 64
+  Matrix & operator *= (const Matrix& arg); // errorcode 64
+  Matrix & operator / (const Matrix& arg); // errorcode 128
+  Matrix & operator /= (const Matrix& arg); // errorcode 128
  private:
   ERRORCODE errorcode;
 };
